3ecd8d908261343417f91d80e64a2a8871a38491
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-27 20:23:54 +0500

    dbContext for car

---
+++ FirstDemoApp/FirstDemoClasstwo/Data/CarDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CarSystemDomain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSystemApplication.Data
+{
+    public class CarDbContext : DbContext
+    {
+        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options) { }
+
+        public virtual DbSet<Car> Cars { get; set; }
+    }
+}
