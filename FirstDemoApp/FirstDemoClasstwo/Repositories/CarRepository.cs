37f8e8c6ee097145a308ba26023d2a4a79324e3f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-27 20:24:22 +0500

    car repositories added

---
+++ FirstDemoApp/FirstDemoClasstwo/Repositories/CarRepository.cs
@@ -0,0 +1,58 @@
+﻿using CarSystemApplication.Data;
+using CarSystemDomain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSystemApplication.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly CarDbContext _carDbContext;
+
+        public CarRepository(CarDbContext carDbContext)
+        {
+            _carDbContext = carDbContext;
+        }
+
+        public Task CreateCar(Car car)
+        {
+            _carDbContext.AddAsync(car);
+            return _carDbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteCar(int id)
+        {
+            var car = _carDbContext.Cars.FirstOrDefault(x=>x.Id == id);
+            _carDbContext.Cars.Remove(car);
+            return _carDbContext.SaveChangesAsync();
+        }
+
+        public Task GetAllCars()
+        {
+            return _carDbContext.Cars.ToListAsync();
+        }
+
+        public Task GetCarById(int id)
+        {
+            return _carDbContext.Cars.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task UpdateCar(Car car)
+        {
+            var lastcar = _carDbContext.Cars.FirstOrDefault(x=>x.Id == car.Id);
+            if(lastcar is null)
+            {
+                return Task.CompletedTask;
+            }
+            lastcar.Name = car.Name;
+            lastcar.Year = car.Year;
+            lastcar.Price = car.Price;
+
+            return _carDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ FirstDemoApp/FirstDemoClasstwo/Repositories/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarSystemDomain;
+
+namespace CarSystemApplication.Repositories;
+
+public interface ICarRepository
+{
+    Task GetAllCars();
+    Task GetCarById(int id);
+    Task UpdateCar(Car car);
+    Task DeleteCar(int id);
+    Task CreateCar(Car car);
+}
