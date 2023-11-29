b51f9a4b4093cf4648facb09895e0a87e9f5e8d8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-29 20:54:02 +0500

    dbcontext modified

--- FirstDemoApp/FirstDemoClasstwo/Data/CarDbContext.cs
+++ FirstDemoApp/FirstDemoClasstwo/Data/CarDbContext.cs
@@ -1,12 +1,31 @@
 ﻿using CarSystemDomain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace CarSystemApplication.Data
 {
     public class CarDbContext : DbContext
     {
-        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options) { }
+        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options)
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
 
-        public virtual DbSet<Car> Cars { get; set; }
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Database.Migrate();
+        }
+
+        public virtual DbSet<Car> MegaCars { get; set; }
     }
 }
