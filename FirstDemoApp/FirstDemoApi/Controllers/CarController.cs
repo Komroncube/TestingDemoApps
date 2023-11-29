4093f2e3574ca92f85b9994c16072eb74a6449a1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-29 20:50:39 +0500

    controller for cars

---
+++ FirstDemoApp/FirstDemoApi/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using CarSystemInfrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllCars()
+        {
+            var cars = await _carService.GetAllCarsAsync();
+            return Ok(cars);
+        }
+    }
+}
