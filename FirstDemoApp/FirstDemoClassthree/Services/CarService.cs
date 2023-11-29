4c845b826a4608940f13961144ce675ef5b3a03e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-29 20:51:56 +0500

    services for car crud

---
+++ FirstDemoApp/FirstDemoClassthree/Services/CarService.cs
@@ -0,0 +1,25 @@
+﻿using CarSystemApplication.Repositories;
+using CarSystemDomain;
+
+namespace CarSystemInfrastructure.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async ValueTask CreateCarAsync(Car car)
+        {
+            await _carRepository.CreateCar(car);
+        }
+
+        public async ValueTask<IEnumerable<Car>> GetAllCarsAsync()
+        {
+            return await _carRepository.GetAllCars();
+        }
+    }
+}
---
+++ FirstDemoApp/FirstDemoClassthree/Services/ICarService.cs
@@ -0,0 +1,10 @@
+﻿using CarSystemDomain;
+
+namespace CarSystemInfrastructure.Services
+{
+    public interface ICarService
+    {
+        ValueTask<IEnumerable<Car>> GetAllCarsAsync();
+        ValueTask CreateCarAsync(Car car);
+    }
+}
